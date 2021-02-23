3346cbae632fe7a1224918d230ce9f33ac39f65b
Ghulam Muhammad <ghulam.m@vidizmo.com>
2021-02-23 14:49:59 +0500

    made changes master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
+
+  myName = "ghulam";
+
+  name() {
+    return this.myName;
+  }
+
+
+shame()
+
+{
+  return "life is all good "
 }
+
+
+  obj = {
+    name: "ghulam lets go ",
+    number: 5
+  };
+
+  siteURL = window.location.href;
+  arr = ["ghulam", "mohammad", "dadabhoy"];
+  a = 100;
+  b = 300;
+
+
+
+
+} // class
